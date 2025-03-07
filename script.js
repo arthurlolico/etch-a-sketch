1ac8976dcda9717258c7c7cbdd5dc085ae93fb2d
Arthur Lolico <arthur.lolico@gmail.com>
2025-03-07 17:43:24 -0300

    Hover effect added

--- script.js
+++ script.js
@@ -8,12 +8,27 @@ function squareItens () {
 
         for (let j = 0; j < 16 ; j++) {
             const square = document.createElement("div");
+            square.style.background = "white";
             square.classList.add("square");
             row.appendChild(square);
         }
 
         container.appendChild(row);
     }
+
+    const grid = document.querySelectorAll(".square");
+
+    grid.forEach((grid) => {
+        grid.addEventListener("mouseenter", function (e) {
+            if(e.target.style.background === "white")
+                e.target.style.background = "black";
+        })
+        grid.addEventListener("mouseleave", function (e) {
+            console.log(e.target.style.background);
+            if(e.target.style.background === "black")
+                e.target.style.background = "white";
+        })
+    })
 }
 
 squareItens();
